72c78a525aceda978fc1bbb77ba364fe2e2db61d
naga pranav <nagapranav.3128t@gmail.com>
2023-10-03 21:12:48 +0530

    commited

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.llms import HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from htmltemplates import css,bot_template,user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -37,13 +38,33 @@ def get_conversation_chain(vectorstore):
         memory=memory
     )
     return conversation_chain
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question':user_question})
+    st.session_state.chat_history  = response["chat_history"]
+
+    for i,message in enumerate(st.session_state.chat_history):
+        if i%2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}",message.content
+            ),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDF's",page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation-None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
     st.header("Chat with multiple PDF's  :books:")
     user_question = st.text_input("Ask a question about your document :")
+    if user_question:
+        handle_userinput(user_question)
     with st.sidebar:
         st.subheader("your documents")
         pdf_docs = st.file_uploader("upload your PDFs hera and click on 'process'",accept_multiple_files=True)
