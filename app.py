e96eae5856c2146d1ad7d36fcd10bee88df2662a
naga pranav <nagapranav.3128t@gmail.com>
2023-10-03 20:42:33 +0530

    commited my files

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import HuggingFaceHub
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+def get_vectorstore(chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=chunks,embeddings=embeddings)
+    return vectorstore
+def get_conversation_chain(vectorstore):
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl",model_kwargs={"temperature":0.5,"max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
+    conversation_chain=ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDF's",page_icon=":books:")
+    st.header("Chat with multiple PDF's  :books:")
+    user_question = st.text_input("Ask a question about your document :")
+    with st.sidebar:
+        st.subheader("your documents")
+        pdf_docs = st.file_uploader("upload your PDFs hera and click on 'process'",accept_multiple_files=True)
+        if st.button("process"):
+            with st.spinner("processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conversation_chain(vector_store)
+                
+
+
+if __name__ == '__main__':
+    main()
